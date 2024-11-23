09d54e5bdbca2492b54d8cc91c0d56e2cb4f0b83
mohan7401647399 <mtm.kcs@gmail.com>
2024-11-23 18:14:15 +0530

    'react-auth0'

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+function App() {
+  const { isLoading, error } = useAuth0()
+  return (
+    <main className='column'>
+      <h1>Auth0 Login</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
+    </main>
+  );
+}
+
+export default App;
